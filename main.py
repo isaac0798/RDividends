525d450bf2fb31412b7cd28f621f4855547c8923
Isaac Tennant <isaactennant@Isaacs-MacBook-Pro.local>
2024-11-06 15:52:29 +0000

    dividends organisaed into dict by year and month

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import csv
 from datetime import datetime
 from collections import defaultdict
 
-dividendsByYear = defaultdict(lambda: defaultdict(int))
+dividendsByYear = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
 
 def get_month_key(date):
     """Convert datetime to YYYY-MM format for grouping"""
@@ -21,8 +21,10 @@ with open('orders.csv', newline='') as csvfile:
     tickers = defaultdict(int)
     for row in spamreader:
         if row['Action'] == 'Dividend (Dividend)':
+            year = get_year_key(row['Time'])
+            month = get_month_key(row['Time'])
 
-            dividendsByYear[get_year_key(row['Time'])][get_month_key(row['Time'])] = {}
+            dividendsByYear[year][month][row['Ticker']] = row['Total']
 
             print(row['Time'], row['Ticker'], row['Total'], get_month_key(row['Time']))
             tickers[row['Name']] += float(row['Total'])
